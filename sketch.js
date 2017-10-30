7e44559796695ebc68713ba08d5dbff6c6d43d21
bethyd <bethyd123@gmail.com>
2017-10-29 23:44:15 -0400

    more seed constants and skeleton functions for seed

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
-function setup() {
+// constants for what a seed does
+var SEED_FALLING = 0;
+var SEED_RESTING = 1;
+var SEED_DIGGING = 2;
+var SEED_GROWING = 3;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
 }
 
 function draw() {
-    
+  
+}
+
+/**
+ * Gives a seed the following functionallity:
+ * falling, resting, digging, growing
+ */ 
+function plantSeed(x, y) {
+  
 }
